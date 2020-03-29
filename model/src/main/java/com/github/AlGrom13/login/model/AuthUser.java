24c5d016ed123b2836ff9a68e7365d10e07692f8
AlGrom13 <shaman_tr@mail.ru>
2020-03-29 21:59:01 +0300

    Add body to class AuthUser

--- model/src/main/java/com/github/AlGrom13/login/model/AuthUser.java
+++ model/src/main/java/com/github/AlGrom13/login/model/AuthUser.java
@@ -1,4 +1,25 @@
 package com.github.AlGrom13.login.model;
 
 public class AuthUser {
+    private String login;
+    private String password;
+    private Role role;
+
+    public AuthUser(String login, String password, Role role) {
+        this.login = login;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
 }
