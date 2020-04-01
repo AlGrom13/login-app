d705aee74ff6d64a4dd0df0641b2773038e8bd3e
AlGrom13 <shaman_tr@mail.ru>
2020-04-01 21:08:39 +0300

    Implement DefaultRegistrationService

---
+++ service/src/main/java/com/github/AlGrom13/login/impl/DefaultRegistrationService.java
@@ -0,0 +1,33 @@
+package com.github.AlGrom13.login.impl;
+
+import com.github.AlGrom13.login.AuthUserDao;
+import com.github.AlGrom13.login.RegistrationService;
+import com.github.AlGrom13.login.model.AuthUser;
+
+public class DefaultRegistrationService implements RegistrationService {
+    private AuthUserDao authUserDao = DefaultAuthUserDao.getInstance();
+
+    private static volatile RegistrationService instance;
+
+    public static RegistrationService getInstance() {
+        RegistrationService localInstance = instance;
+        if (localInstance == null) {
+            synchronized (RegistrationService.class) {
+                localInstance = instance;
+                if (localInstance == null) {
+                    instance = localInstance = new DefaultRegistrationService();
+                }
+            }
+        }
+        return localInstance;
+    }
+
+    @Override
+    public boolean registerUser(AuthUser user) {
+        if (authUserDao.getByLogin(user.getLogin()) == null) {
+            return false;
+        }
+        authUserDao.saveAuthUser(user);
+        return true;
+    }
+}
